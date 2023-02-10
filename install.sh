5dff037200f4e48e986e66bb1412465f17683774
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-02-10 11:03:05 -0500

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-echo "Please remove any aliases you have in your bashrc of zshrc for heck."
-curl -Lo l.zip https://github.com/aarikpokras/heck/archive/refs/heads/rawget.zip
-unzip l.zip -d /usr/local/bin
-cd /usr/local/bin/heck-rawget
-rsync -a * ..
-cd
