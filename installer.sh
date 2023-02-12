2bf4a4a28eeeef53f10f04b48c48bc19c4dd2b00
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-02-11 22:49:53 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,4 +1,4 @@
-echo "Please remove any aliases you have in your bashrc or zshrc for heck."
+echo "$(printf '\033[1;37m')Please remove any aliases you have in your bashrc or zshrc for heck.$(printf '\033[0m')"
 curl -Lo l.zip https://github.com/aarikpokras/heck/archive/refs/heads/main.zip
 unzip l.zip -d /usr/local/bin
 cd /usr/local/bin/heck-main
